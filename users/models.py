f72dc4472dee997c862e4024f8b9d25dc133a8f2
Ashutosh Sharma <ashutoshsharma77332@gmail.com>
2024-04-03 11:42:23 +0530

    first commit

---
+++ users/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+class User(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    address=models.CharField(max_length=100)
+    email=models.EmailField(max_length=30)
+    password=models.CharField(max_length=30)
+    role=models.CharField(max_length=30,default='employee')
+
+class Attendance(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date=models.DateField()
+    in_time=models.TimeField()
+    out_time=models.TimeField()
+
+class Device(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    device_name = models.CharField(max_length=30)
+    device_type = models.CharField(max_length=30)
+    location = models.CharField(max_length=100)    
+
+    asdasd
+
+    # Create your models here.
