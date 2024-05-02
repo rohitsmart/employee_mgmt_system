44322425be6cf57da41e367e57a1d9e989a822db
Ashutosh Sharma <ashutoshsharma77332@gmail.com>
2024-05-02 11:34:27 +0530

    first commit

--- empenv/Scripts/activate.bat
+++ empenv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\91957\Desktop\EmployeeManagementSystem\employee_mgmt_system\empenv
+set VIRTUAL_ENV=D:\django program\rohit\employee_mgmt_system\empenv
 
 if not defined PROMPT set PROMPT=$P$G
 
