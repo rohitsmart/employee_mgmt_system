fb33046834c58bbc4ffc3aaf619f73649edbfe59
manish <manishmmdu@gmail.com>
2024-05-30 11:47:04 +0530

    added authentications

--- adminauth/views.py
+++ adminauth/views.py
@@ -7,76 +7,8 @@ from django.http import JsonResponse
 from django.views.decorators.http import require_POST
 from django.views.decorators.csrf import csrf_exempt
 from project.decorators import jwt_auth_required
-from adminauth.models import UserCredential
 from users.models import User,EmpID
 from django.contrib.auth.hashers import make_password
-
-
-@require_POST
-@csrf_exempt
-@jwt_auth_required
-def create_user_credential(request):
-    try:
-        data = json.loads(request.body)
-        email = data.get('email')
-        if not User.objects.filter(id=request.user_id, role='admin').exists():
-            return JsonResponse({'error': 'Only accessible by Admin'}, status=400)
-        
-        user = User.objects.filter(email=email).first()
-        
-        if user:
-            if user.role == 'candidate':
-                first_name = user.firstName
-                last_name = user.lastName
-                username = generate_username(first_name, last_name)
-            elif user.role == 'employee':
-                full_name = user.fullName
-                empIDdata=user.emp
-                username = generate_employee_username(full_name, empIDdata.emp_id)
-            else:
-                return JsonResponse({'error': 'Invalid user role'}, status=400)
-            
-            password = generate_password()
-
-            if UserCredential.objects.filter(username=username).exists():
-                return JsonResponse({'error': 'Username already exists'}, status=400)
-            elif UserCredential.objects.filter(email=email).exists():
-                return JsonResponse({'error': 'Email already exists'}, status=400)
-            
-            UserCredential.objects.create(
-                username=username,
-                password=password,
-                email=email
-            )
-
-            return JsonResponse({
-                'username': username,
-                'password': password,
-                'email': email
-            })
-        else:
-            return JsonResponse({'error': 'User not found'}, status=404)
-        
-    except Exception as e:
-        return JsonResponse({'error': str(e)}, status=500)
-
-
-def generate_username(first_name, last_name):
-    first_chars = first_name[:1].upper() + first_name[1:2].lower()
-    random_chars = ''.join(random.choices(string.ascii_letters + string.digits, k=5))
-    last_chars = last_name[:1].upper()
-    return first_chars + random_chars + last_chars
-
-def generate_employee_username(full_name, emp_id):
-    first_chars = full_name[:1].upper() + '@'
-    random_chars = ''.join(random.choices(string.ascii_letters + string.digits, k=2))
-    last_chars = str(emp_id)
-    return first_chars + random_chars + last_chars
-
-# def generate_password():
-#     return ''.join(random.choices(string.ascii_letters + string.digits, k=8))
-
-
 
 
 @require_POST
@@ -135,10 +67,16 @@ def generate_password(fullName):
     password = f"{first_part}@{random_number}"
     return password
 
+
+
 @csrf_exempt
+@jwt_auth_required
 def update_user(request):
     if request.method == 'PUT':
         try:
+            if not User.objects.filter(id=request.user_id, role='admin').exists():
+              return JsonResponse({'error': 'Only accessible by Admin'}, status=400)
+            
             user_id = request.GET.get('user_id')
             user = User.objects.get(id=user_id)
             data = json.loads(request.body)
@@ -166,9 +104,13 @@ def update_user(request):
 
 
 @csrf_exempt
+@jwt_auth_required
 def deactivate_employee(request):
     if request.method == 'PUT':
         try:
+            if not User.objects.filter(id=request.user_id, role='admin').exists():
+              return JsonResponse({'error': 'Only accessible by Admin'}, status=400)
+            
             emp_id = request.GET.get('emp_id')
             id = EmpID.objects.filter(emp_id=emp_id).values_list('id', flat=True).first()
             userwithEmpId = User.objects.filter(emp=id)
@@ -190,9 +132,12 @@ def deactivate_employee(request):
 
 
 @csrf_exempt
+@jwt_auth_required
 def delete_employee(request):
     if request.method == 'DELETE':
         try:
+            if not User.objects.filter(id=request.user_id, role='admin').exists():
+               return JsonResponse({'error': 'Only accessible by Admin'}, status=400)
             emp_id = request.GET.get('emp_id')
             emp = EmpID.objects.filter(emp_id=emp_id).first() 
             if emp:
@@ -208,11 +153,13 @@ def delete_employee(request):
 
 
 @csrf_exempt
+@jwt_auth_required
 def reset_user_passwrod(request):
     if request.method == 'PUT':
         try:
             if not User.objects.filter(id=request.user_id, role='admin').exists():
              return JsonResponse({'error': 'Only accessible by Admin'}, status=400)
+            
             user_id = request.GET.get('user_id')
             user = User.objects.get(id=user_id)
             new_password = 'password'
@@ -228,8 +175,10 @@ def reset_user_passwrod(request):
     
 
 @csrf_exempt
+@jwt_auth_required
 def fetch_user(request):
     try:
+        
         if not User.objects.filter(id=request.user_id, role='admin').exists():
             return JsonResponse({'error': 'Only accessible by Admin'}, status=400)
         role = request.GET.get('role')
